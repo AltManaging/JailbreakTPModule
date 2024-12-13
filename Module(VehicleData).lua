9168fc91208512d057440b2fa2a3d97645b674eb
AltManaging <iyrimarler@gmail.com>
2024-12-13 17:03:24 +0000

    Create Module(VehicleData).lua

---
+++ Module(VehicleData).lua
@@ -0,0 +1,584 @@
+-- Decompiler will be improved soon!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/wyButjTMhM)
+-- Decompiled on 2024-12-13 09:05:15
+-- Luau version 6, Types version 3
+-- Time taken: 0.005205 seconds
+
+local module = {{
+	Type = "Chassis";
+	Make = "Manta";
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "Wedge";
+	Season = 23;
+	SeasonIconId = 106450572784202;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "Scorpio";
+	Retired = true;
+	NoTrade = false;
+}, {
+	Type = "Chassis";
+	Make = "Hammerhead";
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "Maverick";
+	Season = 22;
+	SeasonIconId = 18128198711;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "Venom";
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "OGLaMatador";
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "OGMonster";
+	Level = 10;
+	Season = 21;
+	SeasonIconId = 17137444170;
+}, {
+	Type = "Chassis";
+	Make = "XRK";
+	Price = 299000;
+}, {
+	Type = "Chassis";
+	Make = "Striker";
+	Season = 20;
+	SeasonIconId = 16176423092;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "Carbonara";
+	Price = 800000;
+	Retired = true;
+}, {
+	Type = "Chassis3";
+	Make = "SpeedYacht";
+	Price = 250000;
+}, {
+	Type = "Chassis";
+	Make = "FrostCrawler";
+	Season = 19;
+	SeasonIconId = 15624648397;
+	Level = 10;
+}, {
+	Type = "Motorcycle";
+	Make = "FalconS";
+	Price = 150000;
+}, {
+	Type = "Chassis";
+	Make = "Successor";
+	Price = 649000;
+}, {
+	Type = "Chassis";
+	Make = "Laviolette";
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "Longhorn";
+	Season = 18;
+	SeasonIconId = 14952760017;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "Fiasco";
+	Hidden = true;
+	Lite = true;
+}, {
+	Type = "Chassis";
+	Make = "Aperture";
+	Price = 899000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Power1";
+	Season = 17;
+	Level = 10;
+	SeasonIconId = 14279340201;
+}, {
+	Type = "Chassis";
+	Make = "PrisonBus";
+	Price = 150000;
+}, {
+	Type = "Chassis";
+	Make = "Poseidon";
+	Season = 16;
+	Level = 10;
+	SeasonIconId = 13686306316;
+}, {
+	Type = "Chassis";
+	Make = "Shogun";
+	Season = 15;
+	Level = 10;
+	SeasonIconId = 12995659550;
+}, {
+	Type = "Chassis";
+	Make = "NASCAR75th";
+	Hidden = true;
+	NoGarageSpawnUnlessOwned = true;
+}, {
+	Type = "Chassis";
+	Make = "Semi";
+	Price = 100000;
+	Lite = true;
+	NoPhysicalGarageSpawn = true;
+	AllowCustomizeAnywhere = true;
+}, {
+	Type = "Chassis";
+	Make = "Parisian";
+	Price = 720000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Proto8";
+	Season = 14;
+	Level = 10;
+	SeasonIconId = 12425001512;
+}, {
+	Type = "Chassis";
+	Make = "Crown";
+	Price = 666000;
+}, {
+	Type = "Chassis";
+	Make = "Bandit";
+	Season = 13;
+	Level = 10;
+	SeasonIconId = 11811058057;
+}, {
+	Type = "Heli";
+	Make = "EscapeBot";
+	NoMainWorldGarageSpawn = true;
+	Hidden = true;
+}, {
+	Type = "Chassis";
+	Make = "Javelin";
+	Price = 929929;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "DesertCrawler";
+	Price = 229000;
+}, {
+	Type = "Chassis";
+	Make = "ShellMark5";
+	Price = 92000;
+}, {
+	Type = "Chassis";
+	Make = "Goliath";
+	Season = 12;
+	Level = 10;
+	SeasonIconId = 11183663040;
+}, {
+	Type = "Chassis";
+	Make = "ShellClassic";
+	Hidden = true;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Bloxy";
+	Season = 11;
+	Level = 10;
+	SeasonIconId = 10529387816;
+}, {
+	Type = "Chassis";
+	Make = "TinyToy";
+	Season = 11;
+	Level = 8;
+	SeasonIconId = 10529386892;
+}, {
+	Type = "Chassis";
+	Make = "Snake";
+	Price = 649000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Macaron";
+	Season = 10;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "JackRabbit";
+	Season = 9;
+	Level = 8;
+}, {
+	Type = "Chassis";
+	Make = "TowTruck";
+	Price = 80000;
+}, {
+	Type = "Chassis";
+	Make = "MCL36";
+	Price = 200000;
+	Retired = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "MCL36FREE";
+	NoMainWorldGarageSpawn = true;
+	Retired = true;
+	NoTrade = true;
+	NoGarageUI = true;
+}, {
+	Type = "Heli";
+	Make = "Celsior";
+	Season = 8;
+	Level = 10;
+}, {
+	Type = "Chassis";
+	Make = "Badger";
+	Price = 45000;
+}, {
+	Type = "Chassis";
+	Make = "IceBreaker";
+	Price = 500000;
+	Season = 7;
+	Level = 10;
+}, {
+	Type = "Custom";
+	Make = "Tank";
+	Price = 1000000;
+}, {
+	Type = "Chassis";
+	Make = "NASCAR2";
+	Lite = true;
+	Retired = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "NASCAR1";
+	Price = 100000;
+	Retired = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Beignet";
+	Price = 500000;
+	Season = 6;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Concept";
+	Price = 2500000;
+}, {
+	Type = "Chassis";
+	Make = "Torero";
+	Price = 599000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Trailblazer";
+	Price = 1000000;
+}, {
+	Type = "Chassis";
+	Make = "Ray9";
+	Price = 199000;
+}, {
+	Type = "Chassis";
+	Make = "TheCamper";
+	Price = 79000;
+}, {
+	Type = "Chassis";
+	Make = "BananaCar";
+	Price = 100000;
+	Season = 5;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Sentinel";
+	Price = 250000;
+}, {
+	Type = "Chassis";
+	Make = "TheRattler";
+	Price = 100000;
+	Season = 4;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "TheAgent";
+	Price = 600000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Interrogator";
+	Price = 30000;
+}, {
+	Type = "Chassis";
+	Make = "Volt4x4";
+	Price = 100000;
+	Season = 3;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "AirTail";
+	Price = 600000;
+	Retired = true;
+}, {
+	Type = "Heli";
+	Make = "CrewCapsule";
+	Price = 100000;
+	Season = 2;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "BeamHybrid";
+	Price = 100000;
+	Season = 1;
+	Level = 10;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Delorean";
+	Price = 175000;
+}, {
+	Type = "Heli";
+	Make = "Drone";
+	Price = 1000000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Chassis";
+	Make = "Boxer";
+	Price = 55000;
+}, {
+	Type = "Chassis";
+	Make = "MoltenM12";
+	Price = 599000;
+	Retired = true;
+}, {
+	Type = "Heli";
+	Make = "Lia";
+	Price = 1000000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Deja";
+	Price = 10000;
+	Lite = true;
+}, {
+	Type = "Chassis";
+	Make = "Ray";
+	Price = 25000;
+}, {
+	Type = "Chassis";
+	Make = "Challenger";
+	Price = 59000;
+}, {
+	Type = "Chassis";
+	Make = "Surus";
+	Price = 109000;
+}, {
+	Type = "Heli";
+	Make = "LittleBird";
+	Price = 190000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Chassis3";
+	Make = "Sloop";
+	Price = 1000000;
+}, {
+	Type = "Chassis3";
+	Make = "Cruiser";
+	Price = 0;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Chiron";
+	Price = 600000;
+}, {
+	Type = "Chassis";
+	Make = "Cybertruck";
+	Price = 100000;
+}, {
+	Type = "Chassis";
+	Make = "Roadster";
+	Price = 600000;
+}, {
+	Type = "Chassis3";
+	Make = "Jet";
+	Price = 1000000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Chassis3";
+	Make = "Stunt";
+	Price = 300000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Chassis";
+	Make = "Raptor";
+	Price = 150000;
+	Season = 3;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "R8";
+	Price = 200000;
+	Season = 3;
+	Retired = true;
+}, {
+	Type = "Chassis3";
+	Make = "JetSki";
+	Price = 50000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Motorcycle";
+	Make = "Patrol";
+	Price = 45000;
+}, {
+	Type = "Chassis";
+	Make = "Jeep";
+	Price = 0;
+	Lite = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Firetruck";
+	Price = 175000;
+}, {
+	Type = "Chassis";
+	Make = "Arachnid";
+	Price = 750000;
+	Season = 2;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Torpedo";
+	Price = 750000;
+	Season = 2;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Ambulance";
+	Price = 90000;
+}, {
+	Type = "Chassis";
+	Make = "Wraith";
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Classic";
+	Price = 50000;
+	Retired = true;
+}, {
+	Type = "Heli";
+	Make = "BlackHawk";
+	Price = 1000000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Heli";
+	Make = "UFO";
+	Price = 500000;
+	NoMainWorldGarageSpawn = true;
+}, {
+	Type = "Chassis";
+	Make = "SUV";
+	Price = 40000;
+	Retired = true;
+}, {
+	Type = "Custom";
+	Make = "Volt";
+	Price = 1000000;
+}, {
+	Type = "Chassis";
+	Make = "McLaren";
+	Price = 300000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "ATV";
+	Price = 50000;
+}, {
+	Type = "Chassis";
+	Make = "Monster";
+	Price = 1000000;
+}, {
+	Type = "Chassis";
+	Make = "Ferrari";
+	Price = 200000;
+}, {
+	Type = "Chassis";
+	Make = "Mustang";
+	Price = 50000;
+	Retired = true;
+}, {
+	Type = "DuneBuggy";
+	Make = "DuneBuggy";
+	Price = 45000;
+}, {
+	Type = "Motorcycle";
+	Make = "Dirtbike";
+	Price = 35000;
+}, {
+	Type = "Chassis";
+	Make = "Bugatti";
+	Price = 500000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "MiniCooper";
+	Price = 25000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "Porsche";
+	Price = 70000;
+	Retired = true;
+}, {
+	Type = "Chassis";
+	Make = "SWATVan";
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Pickup";
+	Price = 9000;
+	Lite = true;
+}, {
+	Type = "Chassis";
+	Make = "Lamborghini";
+	Price = 100000;
+}, {
+	Type = "Heli";
+	Make = "Heli";
+	Price = 0;
+	NoMainWorldGarageSpawn = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "Model3";
+	Price = 16000;
+	Lite = true;
+}, {
+	Type = "Chassis";
+	Make = "Camaro";
+	Price = 0;
+	Lite = true;
+	NoTrade = true;
+}, {
+	Type = "Chassis";
+	Make = "BankTruck";
+	Price = 0;
+	NoMainWorldGarageSpawn = true;
+	NoGarageUI = true;
+	NoTrade = true;
+}, {
+	Make = "JetPack";
+	Price = 50000;
+	NoMainWorldGarageSpawn = true;
+	NoGarageUI = true;
+	NoTrade = true;
+}}
+for _, v in module do
+	if v.Price ~= nil and v.Price <= 100000 and v.Level ~= 10 and v.Type ~= "Heli" then
+		v.Lite = true
+	end
+end
+return module
